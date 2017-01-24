2086b0e3043b034fbb0f225944296b4340056231
tbrunetti <tonya.brunetti@colorado.edu>
2017-01-24 11:22:26 -0600

    parallelized and working, but not yet streamlined

--- RNA-seq-normalization-DE-analysis.R
+++ RNA-seq-normalization-DE-analysis.R
@@ -7,19 +7,18 @@ biocLite("DESeq2")
 library("DESeq2")
 library(limma)
 library("BiocParallel")
-# number refers to the number or threads/processors to run analysis on (parallelization)
-register(MulticoreParam(7))
-
+register(MulticoreParam(10))
 
 #-----------------gene expression analysis portion (DE)----------------------
-total_raw_counts <- read.table("/home/tonya/Desktop/express_effective_counts_matix", header=TRUE, row.names=1, check.names=FALSE)
-metadata <- read.table("synapse-meta-clinical-technical-data-BrainGVEX-RNAseq.csv", header=TRUE, row.names=1, check.names=FALSE, sep=",")
+total_raw_counts <- read.table("/data/users/tbrunetti/pyschENCODE-DE-analysis/express_effective_counts_matrix_rounded_gene_level.csv", header=TRUE, row.names=432, check.names=FALSE, sep=",")
+total_raw_counts <- total_raw_counts[ , -which(names(total_raw_counts) %in% c("nothing"))]
+metadata <- read.table("/data/users/tbrunetti/pyschENCODE-DE-analysis/synapse-meta-clinical-technical-data-BrainGVEX-RNAseq-TonyaCurrated-V2-MISSING-DATA-FILLED-WITH-AVERAGES.csv", header=TRUE, row.names=1, check.names=FALSE, sep=",")
 total_raw_read_counts_dataframe <- as.data.frame(total_raw_counts)
 metadata_dataframe <- as.data.frame(metadata)
 
 # remove samples now that you don't want to use for analysis
-total_raw_read_counts_dataframe <- within(total_raw_read_counts_dataframe, rm('2015-1', '2015-739', '2015-2883', '2016-1531', '2016-1532'))
-metadata_dataframe <- metadata_dataframe[!rownames(metadata_dataframe) %in% c('2015-1', '2015-739', '2015-2883', '2016-1531', '2016-1532'), ]
+#total_raw_read_counts_dataframe <- within(total_raw_read_counts_dataframe, rm('2015-1', '2015-739', '2015-2883', '2016-1531', '2016-1532'))
+#metadata_dataframe <- metadata_dataframe[!rownames(metadata_dataframe) %in% c('2015-1', '2015-739', '2015-2883', '2016-1531', '2016-1532'), ]
 
 # sorts dataframes so columns and rows are in same BID order for count data and metadata
 counts_sorted <- total_raw_read_counts_dataframe[,order(colnames(total_raw_read_counts_dataframe))]
@@ -29,7 +28,7 @@ metadata_sorted <- metadata_dataframe[order(rownames(metadata_dataframe)),]
 all(rownames(metadata_sorted)==colnames(counts_sorted))
 
 
-deseq_obj <- DESeqDataSetFromMatrix(countData= counts_sorted, colData=metadata_sorted , design = ~ LibraryBatch + FlowcellBatch + BrainBank + UF_MEDIAN_5PRIME_BIAS + UF_MEDIAN_5PRIME_TO_3PRIME_BIAS + UF_MEDIAN_3PRIME_BIAS + RIN + Average_bp_size_of_Library + ERCC_Added + TissueState + Final_Bead_Size_Selection_Ratio + Diagnosis )
+deseq_obj <- DESeqDataSetFromMatrix(countData= counts_sorted, colData=metadata_sorted , design= ~UF_MEDIAN_5PRIME_TO_3PRIME_BIAS + LibraryBatch+ PMI + Sex + BrainBank + RIN +Diagnosis )
 #set comparison reference/control
 print("Releveling reference to Control")
 deseq_obj$Diagnosis <- relevel(factor(deseq_obj$Diagnosis), ref="Control")
@@ -38,28 +37,66 @@ deseq_obj$Diagnosis <- relevel(factor(deseq_obj$Diagnosis), ref="Control")
 deseq_obj <- deseq_obj[rowSums(counts(deseq_obj)) > 1, ]
 
 # perform DE analysis (size factors, dispersion, negative binomial distribution)
-deseq_obj <- DESeq(deseq_obj, parallel = TRUE)
+deseq_obj <- DESeq(deseq_obj, parallel=TRUE)
 
-de_results <- results(deseq_obj, parallel = TRUE)
-resOrdered <- de_results[order(de_results$padj),]
-summary(results)
-write.csv(as.data.frame(resOrdered), file="/data/users/tbrunetti/UC-UIC-covariates-added-to-model-testing-condition.csv")
+de_results_control_bp <- results(deseq_obj, parallel=TRUE, contrast=c("Diagnosis", "BP", "Control"))
+de_results_control_scz <- results(deseq_obj, parallel=TRUE, contrast=c("Diagnosis", "SCZ", "Control"))
+de_results_bp_scz <- results(deseq_obj, parallel=TRUE, contrast=c("Diagnosis", "BP", "SCZ"))
 
-#those adj p-value of <0.05
-res05 <- results(deseq_obj, alpha=0.05)
-summary(res05)
-adj05 <- subset(resOrdered, padj < 0.05)
-write.csv(as.data.frame(adj05), file="/data/users/tbrunetti/UC-UIC-covariates-added-to-model-testing-condition-less-pval-0.05.csv")
+resOrdered_c_bp <- de_results_control_bp[order(de_results_control_bp$padj),]
+summary(de_results_control_bp)
+#write.csv(as.data.frame(resOrdered_c_bp), file="/data/users/tbrunetti/pyschENCODE-DE-analysis/results/UC-UIC-covariates-added-to-model-testing-condition-all-430-samples-PMI-sex-brainbank-53bias-libraryBatch-RIN-Diagnosis-control_vs_bp.csv")
 
 
-#those adj p-value of <0.01
-res01 <- results(deseq_obj, alpha=0.01)
-summary(res01)
-adj01 <- subset(resOrdered, padj < 0.01)
-write.csv(as.data.frame(adj01), file="/data/users/tbrunetti/UC-UIC-covariates-added-to-model-testing-condition-less-pval-0.01.csv")
+resOrdered_c_scz <- de_results_control_scz[order(de_results_control_scz$padj),]
+summary(de_results_control_scz)
+#write.csv(as.data.frame(resOrdered_c_scz), file="/data/users/tbrunetti/pyschENCODE-DE-analysis/results/UC-UIC-covariates-added-to-model-testing-condition-all-430-samples-PMI-sex-brainbank-53bias-libraryBatch-RIN-Diagnosis-control_vs_scz.csv")
 
+
+resOrdered_bp_scz <- de_results_bp_scz[order(de_results_bp_scz$padj),]
+summary(de_results_bp_scz)
+#write.csv(as.data.frame(resOrdered_bp_scz), file="/data/users/tbrunetti/pyschENCODE-DE-analysis/results/UC-UIC-covariates-added-to-model-testing-condition-all-430-samples-PMI-sex-brainbank-53bias-libraryBatch-RIN-Diagnosis-bp_vs_scz.csv")
+
+#those adj p-value of <0.05 control vs BP
+res05_c_bp <- results(deseq_obj, alpha=0.05, contrast=c("Diagnosis", "BP", "Control"))
+summary(res05_c_bp)
+adj05_c_bp <- subset(resOrdered_c_bp, padj < 0.05)
+#write.csv(as.data.frame(adj05_c_bp), file="/data/users/tbrunetti/pyschENCODE-DE-analysis/results/UC-UIC-covariates-added-to-model-testing-condition-all-430-samples-PMI-sex-brainbank-53bias-libraryBatch-RIN-Diagnosis-control_vs_bp_less-pval-0.05.csv")
+
+
+#those adj p-value of <0.05 control vs scz
+res05_c_scz <- results(deseq_obj, alpha=0.05, contrast=c("Diagnosis", "SCZ", "Control"))
+summary(res05_c_scz)
+adj05_c_scz <- subset(resOrdered_c_scz, padj < 0.05)
+#write.csv(as.data.frame(adj05_c_scz), file="/data/users/tbrunetti/pyschENCODE-DE-analysis/results/UC-UIC-covariates-added-to-model-testing-condition-all-430-samples-PMI-sex-brainbank-53bias-libraryBatch-RIN-Diagnosis-control_vs_scz_less-pval-0.05.csv")
+
+#those adj p-value of <0.05 BP vs SCZ
+res05_bp_scz <- results(deseq_obj, alpha=0.05, contrast=c("Diagnosis", "BP", "SCZ"))
+summary(res05_bp_scz)
+adj05_bp_scz <- subset(resOrdered_bp_scz, padj < 0.05)
+#write.csv(as.data.frame(adj05_bp_scz), file="/data/users/tbrunetti/pyschENCODE-DE-analysis/results/UC-UIC-covariates-added-to-model-testing-condition-all-430-samples-PMI-sex-brainbank-53bias-libraryBatch-RIN-Diagnosis-bp_vs_scz_less-pval-0.05.csv")
+
+
+#those adj p-value of <0.01 control vs BP
+res01_c_bp <- results(deseq_obj, alpha=0.01, contrast=c("Diagnosis", "BP", "Control"))
+summary(res01_c_bp)
+adj01_c_bp <- subset(resOrdered_c_bp, padj < 0.01)
+#write.csv(as.data.frame(adj01_c_bp), file="/data/users/tbrunetti/pyschENCODE-DE-analysis/results/UC-UIC-covariates-added-to-model-testing-condition-all-430-samples-PMI-sex-brainbank-53bias-libraryBatch-RIN-Diagnosis-control_vs_bp_less-pval-0.01.csv")
+
+
+#those adj p-value of <0.01 control vs scz
+res01_c_scz <- results(deseq_obj, alpha=0.01, contrast=c("Diagnosis", "SCZ", "Control"))
+summary(res01_c_scz)
+adj05_c_scz <- subset(resOrdered_c_scz, padj < 0.01)
+#write.csv(as.data.frame(adj05_c_scz), file="/data/users/tbrunetti/pyschENCODE-DE-analysis/results/UC-UIC-covariates-added-to-model-testing-condition-all-430-samples-PMI-sex-brainbank-53bias-libraryBatch-RIN-Diagnosis-control_vs_scz_less-pval-0.01.csv")
+
+#those adj p-value of <0.01 BP vs SCZ
+res01_bp_scz <- results(deseq_obj, alpha=0.01, contrast=c("Diagnosis", "BP", "SCZ"))
+summary(res01_bp_scz)
+adj01_bp_scz <- subset(resOrdered_bp_scz, padj < 0.01)
+#write.csv(as.data.frame(adj01_bp_scz), file="/data/users/tbrunetti/pyschENCODE-DE-analysis/results/UC-UIC-covariates-added-to-model-testing-condition-all-430-samples-PMI-sex-brainbank-53bias-libraryBatch-RIN-Diagnosis-bp_vs_scz_less-pval-0.01.csv")
 
 
 # output normalized count data
 norm_counts <- counts(deseq_obj, normalized=TRUE)
-write.csv(norm_counts, file='/data/users/tbrunetti/UC-UIC-deseq2-normalized-counts-added-covariates-to-model.csv')
+write.csv(norm_counts, file='/data/users/tbrunetti/pyschENCODE-DE-analysis/results/UC-UIC-deseq2-normalized-counts-all-430-samples-PMI-sex-brainbank-53bias-libraryBatch-RIN-Diagnosis-only-in-model.csv')
